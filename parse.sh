8b8d53cf1c9320e11721f222eecedf0b05e981f3
Twobob <seeTwobob@live.com>
2013-01-19 15:56:47 +0000

    refactored out all references to old JSON parsing classes into a sh script. very basic implementation with TreeMap to handle the instrinsic Button name ¬ command args relationship. removed all references to external jars from previous commit.

---
+++ parse.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+EXTENSIONDIR=/mnt/us/extensions/
+FILES=$EXTENSIONDIR*
+labeltext=""
+stamper=""
+temp=""
+seperator="¬"
+dirsuffix="/"
+prettyseperator=":"
+
+## If you get weird output from the next bit - like you are missing a label?? Run dos2unix on the config.xml 
+## Komic's is broken dos format... Named and shamed... Took me 4 hours to find that bug... ;)
+if [ -e /mnt/us/extensions/komic/config.xml ]
+then
+dos2unix /mnt/us/extensions/komic/config.xml
+fi
+
+for f in $FILES; do
+FILES2=$f/menu.json
+for j in $FILES2; do
+
+## Parse the JSON best we can using a shell. Ditch various bit we aren't concerned with. Only return the action nodes' values
+for z in $(cat $j | sed -e 's/[{}]/''/g' \
+| awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' \
+| sed 's/\"\:\"/\|/g;s/[\,]/ /g;s/\"//g'| grep "action" | grep -w " action") 
+do 
+
+## better clear the params results if we have any
+holder=""
+for params in $(cat $j | grep $z \
+| sed -e 's/[{}]/''/g' \
+| awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' \
+| sed 's/\"\:\"/\|/g;s/[\,]/ /g;s/\"//g'| grep "params")
+do
+
+## and populate the param results if we have any
+paramholder="$holder""`echo $params | sed 's/params://g'` "
+done
+
+## lots of silly reverse logic required for this caper, try not to be horrified
+if [ ! $z  == "action:"  ]
+then
+
+## check the matching config.xml exist and carry on if we can find one
+	if [ -e $f/config.xml ]
+	then
+
+	## create a unique "tidy label" suffix - one per command to discern buttons easily - trim off as much as we can from the name
+	temp=$(echo $z | sed "s-$f--g")
+	thisstamper=$(cat $f/config.xml | grep "<name>" | sed 's/<name>//g;s/<\/name>//g;s/^[ \t]*//;s/[ \t]*$//')
+
+	##make a tidy label based on the config name paramater
+	labeltext=$thisstamper":"$temp
+	else
+	labeltext=$(echo $f | sed "s-$EXTENSIONDIR--g")
+	echo "OOOPS!!! This Happened!!!"
+	fi
+
+## make a determination about appending prefixes to the calls to make them valid.
+## then create a complete call structured
+##
+## Name: suffix ¬ commands <param1> <paramN> ...
+##
+## Todo parse this data in java to make a simplified way to access the old data.
+if [ -e `echo $z | sed 's/ action: //g'` ]
+then
+echo "$labeltext ""¬""`echo $z | sed 's/ action: //g'`$holder"
+else
+stamper=$(cat $f/config.xml | grep "<name>" | sed 's/<name>//g;s/<\/name>//g;s/^[ \t]*//;s/[ \t]*$//')
+tamper="`echo "$z" | sed 's/ action: //g'`"
+echo $stamper "$prettyseperator" "$z" "$seperator" "$f""$dirsuffix""$tamper" "$paramholder"
+fi
+fi
+# meh
+holder=""
+paramholder=""
+done
+done
+done
--- src/com/mobileread/ixtab/kindlelauncher/LauncherKindlet.java
+++ src/com/mobileread/ixtab/kindlelauncher/LauncherKindlet.java
@@ -1,70 +1,39 @@
 package com.mobileread.ixtab.kindlelauncher;
 
-import ixtab.jailbreak.Jailbreak;
-
 import java.awt.BorderLayout;
-//import java.awt.Button;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.GridLayout;
+import ixtab.jailbreak.Jailbreak;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
-//import java.io.IOException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.security.AllPermission;
 import java.util.ArrayList;
-import java.util.Iterator;
-
-import javax.swing.JButton;
+import java.util.TreeMap;
 
 import com.amazon.kindle.kindlet.AbstractKindlet;
 import com.amazon.kindle.kindlet.KindletContext;
-//import com.mobileread.ixtab.kindlelauncher.util.FileUtil;
-import com.mobileread.ixtab.kindlelauncher.util.ExtensionsLoader;
-import com.mobileread.ixtab.kindlelauncher.util.Extension;
 import com.mobileread.ixtab.kindlelauncher.ui.UIAdapter;
 
 public class LauncherKindlet extends AbstractKindlet implements ActionListener {
 
-	private final Jailbreak jailbreak = new Jailbreak();
-
-	private KindletContext context;
-	private Container panel;
-	private String runthing;
-
-	// temporary, for testing.
-	private Component button1, button2, status;
-
 	private static final long serialVersionUID = 1L;
 
-	private File mScript;
-	private String mArgs;
-
-	/**
-	 * Creates a new launch script menu item.
-	 * 
-	 * @param name
-	 *            The text to show
-	 * @param priority
-	 *            The order of this item in comparison to others
-	 * @param script
-	 *            The shell script or command to run
-	 * @param args
-	 *            The arguments of the script
-	 */
-	// public LauncherScript(String name, int priority, File script, String
-	// args) {
-	// super(name, priority);
-
-	// }
-
-	private ArrayList mExtensions;
-	/**
-	 * The default {@link ExtensionsLoader} that reads from /mnt/us/extensions
-	 */
-	private static final ExtensionsLoader EXTENSIONS_LOADER = new ExtensionsLoader(
-			new File("/mnt/us/extensions"));
-
+	private final Jailbreak jailbreak = new Jailbreak();
+	private KindletContext context;
+	private Container panel;
+	private TreeMap tm = new TreeMap();
+	
+	// temporary, for testing.
+	private Component status;
+	
 	public void create(KindletContext context) {
 		this.context = context;
 	}
@@ -74,88 +43,89 @@ public class LauncherKindlet extends AbstractKindlet implements ActionListener {
 		super.destroy();
 	}
 
+	
+	
 	public void start() {
+	
 		// just in case, it's probably not needed.
 		if (panel == null) {
 			panel = getUI().newPanel(new BorderLayout());
 		} else {
 			panel.removeAll();
 		}
-
-		// FIXME We want to grab out the values parsed and run them like below.
-
-		// this.mScript = script;
-		// this.mArgs = args;
-
-		/**
-		 * Let's setup some useful file readers for the future
-		 */
-
-		// FileUtil filer = new FileUtil();
-		runthing = "/mnt/us/usr/bin/soundkloud";
-
-		jailbreak.enable();
-		jailbreak.getContext().requestPermission(new AllPermission());
-
+		
 		Container root = context.getRootContainer();
 		root.setLayout(new BorderLayout());
 		// again, just in case
 		root.removeAll();
-
+		
 		root.add(panel, BorderLayout.CENTER);
-
-		try {
-
-			/**
-			 * Loads the extensions using {@link ExtensionsLoader}
-			 */
-			// FIXME GAH!!! why is this returning no results???
-			mExtensions = EXTENSIONS_LOADER.loadExtensions(); //
-
-		} catch (Throwable ex) {
-			root.add(getUI().newLabel("exception!!: " + ex.getMessage()),
-					BorderLayout.NORTH);
-		}
-
+		
 		/* everything below here is experimental. */
-		root.add(getUI().newLabel("sample label: north"), BorderLayout.NORTH);
+		root.add(getUI().newLabel("LIST OF APPLICATIONS: (click to run)"), BorderLayout.NORTH);
 
-		// those "200" bits are also only for testing.
-		// GridLayout grid = new GridLayout(2, 1, 200, 200);
-
-		GridLayout grid = new GridLayout(mExtensions.size() + 2, 1);
+		GridLayout grid = new GridLayout(0, 1);
 		Container buttonsPanel = getUI().newPanel(grid);
+		
+		jailbreak.enable();
+		jailbreak.getContext().requestPermission(new AllPermission());
 
-		button1 = getUI().newButton("BUTTON 1", this);
-
+		
 		try {
+			
+			File file_location = new File("/mnt/us/documents");
+			
+			String cmd[] = new String[] {"/bin/sh", "parse.sh"}; 
+			
+			Runtime rtime = Runtime.getRuntime();
+			Process processer = rtime.exec(cmd,null,file_location);
+			
+			processer.waitFor();
 
-			if (!mExtensions.isEmpty()) {
-				Iterator it = mExtensions.iterator();
-				while (it.hasNext()) {
-					Extension ext = (Extension) it.next();
-					buttonsPanel.add(
-							getUI().newButton(
-									ext.getId() + ") " + ext.getName(), this),
-							BorderLayout.NORTH);
-				}
-			} else {
-				buttonsPanel.add(getUI().newButton("was empty", this),
-						BorderLayout.NORTH);
+		    String line;
 
-			}
+		    /* Meh we could do this... processer.getErrorStream() But we don't want the trash
+		     * */
 
-		} catch (Throwable ex) {
-		}
+	            BufferedReader input = new BufferedReader(new InputStreamReader(processer.getInputStream()));
+	            while((line=input.readLine()) != null){
+	                
+	                /* No doubt there is a better way */
+	                String item[]=split2(line,"¬");  
+	                
+	                /* link the name to the button label */
+	            	Component looper = getUI().newButton(item[0], this);
+	                
+	            	/* Making a list */
+	            	tm.put(item[0], item[1]);
+	            	
+	            	/* a point of reference, meh, it'll do */
+	            	looper.setName(item[0]);
+	            	
+	                buttonsPanel.add(looper);
+	            }
 
-		button2 = getUI().newButton("BUTTON 2", this);
-		buttonsPanel.add(button1);
-		buttonsPanel.add(button2);
+	            input.close();
+			
+	            /* Just in case... */
+	            OutputStream outputStream = processer.getOutputStream();
+	            PrintStream printStream = new PrintStream(outputStream);
+	            printStream.println();
+	            printStream.flush();
+	            printStream.close();
+
+	            
+            } catch(Exception e) {
+            e.printStackTrace();
+           
+        }
 
 		root.add(buttonsPanel, BorderLayout.CENTER);
-
-		status = getUI().newLabel("sample label: south - STATUS");
+		
+		status = getUI().newLabel("sample label: south");
 		root.add(status, BorderLayout.SOUTH);
+		
+		setStatus(String.valueOf(tm.size()));
 	}
 
 	public void stop() {
@@ -164,43 +134,36 @@ public class LauncherKindlet extends AbstractKindlet implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		Object src = e.getSource();
-		// if (src == button1) {
-		// setStatus("button 1 was pressed");
-		//
-		// } else if (src == button2) {
-		// setStatus("button 2 was pressed");
+		Component src = (Component) e.getSource();
 
-		//FIXME Currently this does nothing useful. Just holds a bit of logic to nastily grab values to status from a button on a 5.
-		
-		try {
-			// This kinda thing???
-			// Runtime.getRuntime().exec(((Button)src).getLabel().trim());
+		String namer = src.getName();
+	
+		setStatus(namer);
 
-			// FIXME Make this grab the right type from the UI adaptor
-			String report = ((JButton) src).getText();
-
-			setStatus(report);
-
-		} catch (NullPointerException ex) {
-			String report = ex.getMessage();// .replaceAll("\\n", "");
-			setStatus(report);
-		} catch (SecurityException ex) {
-			String report = ex.getMessage();// .replaceAll("\\n", "");
-			setStatus(report);
-		}
-		/*
-		 * catch (IOException ex) { String report =
-		 * ex.getMessage();//.replaceAll("\\n", ""); setStatus(report); //
-		 * setStatus(ex.getMessage()); }
-		 */
+	      String runner = (String)tm.get(namer);
+	      setStatus(runner);
+	      try {
+				
+	      Runtime.getRuntime().exec(runner);
+	      } catch (NullPointerException ex) {
+				String report = ex.getMessage();// .replaceAll("\\n", "");
+				setStatus(report);
+			} catch (SecurityException ex) {
+				String report = ex.getMessage();// .replaceAll("\\n", "");
+				setStatus(report);
+			} catch (IOException ex) {
+				String report = ex.getMessage();//.replaceAll("\\n", ""); setStatus(report); //
+			 setStatus(report);
+			 } 
 		catch (Throwable ex) {
-			String report = ex.getMessage();// .replaceAll("\\n", "");
-			setStatus(report);
-		}
-		// };
-	}
+				String report = ex.getMessage();// .replaceAll("\\n", "");
+				setStatus(report);
+			}
 
+	     // one ride per customer... ???
+	     stop();
+	}
+	
 	private void setStatus(String text) {
 		getUI().setText(status, text);
 	}
@@ -209,4 +172,31 @@ public class LauncherKindlet extends AbstractKindlet implements ActionListener {
 	private static UIAdapter getUI() {
 		return UIAdapter.INSTANCE;
 	}
+	
+		
+	// Fixup the lack of handy split method.
+	
+	   public static String[] split2(String input, String separator){
+    	
+    	int separatorlen = separator.length();
+    	
+    	ArrayList arrAux = new ArrayList();
+    	String sAux = "" + input;
+    	int pos = sAux.indexOf(separator);
+    	while (pos>=0){
+    		String token = sAux.substring(0,pos);
+    		arrAux.add(token);
+    		sAux = sAux.substring(pos+separatorlen);
+    		pos = sAux.indexOf(separator);
+    	}
+    	if (sAux.length()>0)
+    		arrAux.add(sAux);
+    	String[] res = new String[arrAux.size()];
+    	for (int i = 0; i<res.length; i++){
+    		res[i] = (String)arrAux.get(i);
+    	}
+    	return res;
+    }
+	
+	
 }
