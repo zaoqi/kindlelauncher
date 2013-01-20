5c2ff44b78b92821547c4072ed115f968682616a
Twobob <seeTwobob@live.com>
2013-01-20 22:08:38 +0000

    refactored logic in shell script to be more easily followable updated comments

--- parse.sh
+++ parse.sh
@@ -8,13 +8,6 @@ seperator="¬"
 dirsuffix="/"
 prettyseperator=":"
 
-## If you get weird output from the next bit - like you are missing a label?? Run dos2unix on the config.xml 
-## Komic's is broken dos format... Named and shamed... Took me 4 hours to find that bug... ;)
-#if [ -e /mnt/us/extensions/komic/config.xml ]
-#then
-#dos2unix /mnt/us/extensions/komic/config.xml
-#fi
-
 for f in $FILES; do
 FILES2=$f/menu.json
 for j in $FILES2; do
@@ -24,6 +17,12 @@ for z in $(cat $j | sed -e 's/[{}]/''/g' \
 | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' \
 | sed 's/\"\:\"/\|/g;s/[\,]/ /g;s/\"//g'| grep "action" | grep -w " action") 
 do 
+
+## reinverted logic required, less horrific. skip ACTION: label headers.
+if [ $z  == "action:"  ]
+then
+continue
+fi
 
 ## better clear the params results if we have any
 holder=""
@@ -37,23 +36,19 @@ do
 paramholder="$holder""`echo $params | sed 's/params://g'` "
 done
 
-## lots of silly reverse logic required for this caper, try not to be horrified
-if [ ! $z  == "action:"  ]
-then
-
-## check the matching config.xml exist and carry on if we can find one
+## check the matching config.xml exist and do a prefixed label if we find the value
 	if [ -e $f/config.xml ]
 	then
 
 	## create a unique "tidy label" suffix - one per command to discern buttons easily - trim off as much as we can from the name
 	temp=$(echo $z | sed "s-$f--g")
 	thisstamper=$(cat $f/config.xml | grep "<name>" | sed 's/<name>//g;s/<\/name>//g;s/^[ \t]*//;s/[ \t]*$//')
-
+	
 	##make a tidy label based on the config name paramater
-	labeltext=$thisstamper":"$temp
+	labeltext="$thisstamper"":""$temp"
 	else
 	labeltext=$(echo $f | sed "s-$EXTENSIONDIR--g")
-	echo "OOOPS!!! This Happened!!!"
+	echo "error! mangle happened ¬ ./menu-error"
 	fi
 
 ## make a determination about appending prefixes to the calls to make them valid.
@@ -69,7 +64,6 @@ else
 stamper=$(cat $f/config.xml | grep "<name>" | sed 's/<name>//g;s/<\/name>//g;s/^[ \t]*//;s/[ \t]*$//')
 tamper="`echo "$z" | sed 's/ action: //g'`"
 echo $stamper "$prettyseperator" "$z" "$seperator" "$f""$dirsuffix""$tamper" "$paramholder"
-fi
 fi
 # meh
 holder=""
