27c1ef1ea4ca0b47da9f77a46b0c57ee4967b022
NiLuJe <ninuje@gmail.com>
2014-02-15 17:22:27 +0100

    Slight rework of the gawk handling.
    Kill the wrapper, and do the version check earlier (before the I/O
    redir), so that out error diagnostic makes it through to ant.

--- src/com/mobileread/ixtab/kindlelauncher/resources/tool/gawk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# We rely on GNU Awk 4 at the very least.
-if [[ "$(env gawk -V 2>/dev/null | grep -e 'GNU Awk' | cut -c 9)" -lt "4" ]] ; then
-	echo "* Your GNU Awk version is too old! We need GNU Awk 4 ;)"
-	exit 1
-fi
-
-exec env gawk -f $*
