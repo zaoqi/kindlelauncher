2ba34cd2896e9296b89199497520dadac70dbb57
NiLuJe <ninuje@gmail.com>
2014-02-15 13:23:20 +0100

    Add a version check to the gawk wrapper.
    Confirmed than nothing (bb, mawk, gawk 3) except gawk 4 does the job...

--- src/com/mobileread/ixtab/kindlelauncher/resources/tool/gawk.sh
+++ src/com/mobileread/ixtab/kindlelauncher/resources/tool/gawk.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-exec env gawk -f $*
+# We rely on GNU Awk 4 at the very least.
+if [[ "$(env awk -V 2>/dev/null | head -n 1 | sed -re 's/^(GNU Awk )([[:digit:]])([[:digit:].]*?)(,)(.*?)$/\2/')" -lt "4" ]] ; then
+	echo "* Your GNU Awk version is too old! We need GNU Awk 4 ;)"
+	exit 1
+fi
 
+exec env gawk -f $*
