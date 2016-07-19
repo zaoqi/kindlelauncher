a3fe23869afa1a0c2d0f2833c7e28181d7ac4d35
NiLuJe <ninuje@gmail.com>
2016-07-19 12:28:11 +0200

    Fix lamest c/p mistake ever. sigh.
    Thanks to chrox for catching that one ;).

--- src/com/mobileread/ixtab/kindlelauncher/resources/booklet/uninstall/appreg.uninstall.sql
+++ src/com/mobileread/ixtab/kindlelauncher/resources/booklet/uninstall/appreg.uninstall.sql
@@ -1,6 +1,6 @@
-DELETE FROM "handlerIds" WHERE handlerId='com.github.koreader.kpvbooklet' or handlerId='com.mobileread.ixtab.kindlelauncher';
-DELETE FROM "properties" WHERE handlerId='com.github.koreader.kpvbooklet' or handlerId='com.mobileread.ixtab.kindlelauncher';
-DELETE FROM "associations" WHERE handlerId='com.github.koreader.kpvbooklet' or handlerId='com.mobileread.ixtab.kindlelauncher';
+DELETE FROM "handlerIds" WHERE handlerId='com.mobileread.ixtab.kindlelauncher';
+DELETE FROM "properties" WHERE handlerId='com.mobileread.ixtab.kindlelauncher';
+DELETE FROM "associations" WHERE handlerId='com.mobileread.ixtab.kindlelauncher';
 
 DELETE FROM "mimetypes" WHERE ext='kual';
 DELETE FROM "extenstions" WHERE ext='kual';
